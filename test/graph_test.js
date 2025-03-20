329bf043264d68cc7d0923f7467d205d8e4b26da
ColinLug <colin.lugin@gmail.com>
2025-03-20 12:36:29 +0100

    cosmétiques

--- test/graph_test.js
+++ test/graph_test.js
@@ -1,84 +1,4 @@
 
-// var cy = cytoscape({
-
-//   container: document.getElementById('cy'), // container to render in
-
-//   elements: [ // list of graph elements to start with
-//     {data:{id: "-1"}},
-//     {data:{id: "0"}},
-//     {data:{id: "1"}},
-//     {data:{id: "2"}},
-//     {data:{id: "3"}},
-//     {data:{id : "e1", source : "1", target : "2"}},
-//     {data:{id : "e2", source : "-1", target : "3"}},
-//     {data:{id : "e3", source : "0", target : "1"}},
-//     {data:{id : "e4", source : "1", target : "-1"}},
-//   ],
-
-//   style: [ // the stylesheet for the graph
-//     {
-//       selector: 'node',
-//       style: {
-//         'background-color': '#666',
-//         'label': 'data(id)'
-//       }
-//     },
-
-//     {
-//       selector: 'edge',
-//       style: {
-//         'width': 3,
-//         'line-color': '#ccc',
-//         'target-arrow-color': '#ccc',
-//         'target-arrow-shape': 'triangle',
-//         'curve-style': 'bezier'
-//       }
-//     }
-//   ],
-
-//   layout: {
-//     name: 'circle',
-//     rows: 1
-//   }
-
-// });
-// class Graph{
-//     constructor(nbVertices, links={}, passage_texts={} ){
-//         this.nbVertices = nbVertices;
-//         this.links = links;
-//         this.passage_texts = passage_texts
-//     }
-//     showVertices(){
-//         for(let i = 1; i<=this.nbVertices; i++){
-//             let vertice = document.createElement("h1")
-//             vertice.setAttribute("id", i)
-//             vertice.setAttribute("passage_text", this.passage_texts )
-//             vertice.addEventListener("click",()=>{
-//                 let text_div = document.body.querySelector("div")
-//                 text_div.textContent=this.passage_texts[i]
-//             })
-//             vertice.textContent = String(i)
-//             document.body.appendChild(vertice)
-//         }
-//     }
-//     async showGraph(){
-//         console.log("bjr")
-//         let blop = await this.showVertices()
-//         for(let i = 1; i<=this.nbVertices; i++){
-//             if(this.links[i]){
-//                 for(let j in this.links[i]){
-//                     let salut = document.getElementById(String(i))
-//                     salut.setAttribute("class", "green")
-
-//                 }
-//             }
-//         }
-//     }
-// }
-// graph = new Graph(90, {3:[1,5]}, {3: "Je suis le monsieur Gris et je te donne une épée.", 7: "Combat les monstres méchants"})
-// graph.showGraph()
-
-
 cy_list = [{data: { id: -1}}]
 for(let i = 0; i<400;i++){
   cy_list.push({data: { id: String(i) }})
@@ -97,8 +17,15 @@ var cy = cytoscape({
     {
       selector: 'node',
       style: {
-        'background-color': 'blue',
-        'label': 'data(id)'
+        'background-color': '#FFFACD',
+        'border-width':'1',
+        'border-color': 'black',
+        'border-opacity':'1',
+        'label': 'data(id)',
+        'text-valign':'center',
+        'text-halign':'center',
+        // 'font-size':10,
+        // 'font-family':'Serif'
       }
     },
 
