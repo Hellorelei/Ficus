78a7a5fb894a0a07c1800d2173bf03bbc01ff5e1
matBorgeaud <115068282+matBorgeaud@users.noreply.github.com>
2025-03-20 15:12:34 +0100

    side pannel

--- test/graph_test.js
+++ test/graph_test.js
@@ -1,13 +1,71 @@
+async function getCSV(url) {
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Erreur: ${response.status}`);
+    }
+    let text_csv = await response.text();
+    let lines = text_csv.split('\n');
+    let headers = lines[0].split(',')
+    let result = []
+    for (let i = 1; i < lines.length; i++) {
+      const obj = {};
+      const currentLine = lines[i].split(',');
 
-cy_list = [{data: { id: -1}}]
-for(let i = 0; i<400;i++){
-  cy_list.push({data: { id: String(i) }})
-}
-for(let i = 0; i<600;i++){
-  cy_list.push({data: { id: `e${String(i)}`, source: `${Math.round(Math.random()*399)}`, target: `${Math.round(Math.random()*399)}` }})
+      // Assurer que la ligne n'est pas vide
+      if (currentLine.length === headers.length) {
+        for (let j = 0; j < headers.length; j++) {
+          if(currentLine[0]==""){
+
+          }
+          obj[headers[j].trim()] = currentLine[j].trim();
+        }
+        result.push(obj);
+      }
+    }
+    return result;
+  } catch (error){
+    console.error('Erreur lors de la récupération ou du traitement du CSV:', error);
+    return null;
+  }
+};
+
+function createSampleCy(){
+  cy_list = [{data: { id: -1}}]
+  for(let i = 0; i<100;i++){
+    cy_list.push({data: { id: String(i) }})
+  }
+  for(let i = 0; i<180;i++){
+    cy_list.push({data: { id: `e${String(i)}`, source: `${Math.round(Math.random()*99)}`, target: `${Math.round(Math.random()*99)}` }})
+  }
+  return cy_list
 }
 
-var cy = cytoscape({
+function destroySideTab() {
+  const element = document.getElementById("sideTab");
+  if (element) {
+    element.remove();
+  }
+}
+
+function newTabOnClick(nodeID) {
+  console.log("Salut" + nodeID)
+  
+  destroySideTab()
+
+  const div = document.createElement("div");
+    div.id = "sideTab" ;
+    div.className = "sideTab";
+    document.body.appendChild(div);
+}
+
+cy_list=createSampleCy()
+
+console.log("Bonjour")
+let csv = getCSV("A_COPIER_labyrinthe_de_la_mort - template_ldvelh.csv")
+console.log(csv)
+
+let cy = cytoscape({
 
   container: document.getElementById('cy'), // container to render in
 
@@ -47,3 +105,24 @@ var cy = cytoscape({
   }
 
 });
+//event listener on node click
+cy.on('click', 'node', function(evt){
+  console.log( 'clicked ' + this.id() );
+  nodeID = this.id()
+  newTabOnClick(nodeID)
+});
+
+
+cy.on('click', function(event) {
+  // Vérifie si l'élément cliqué est le fond (pas un node)
+  if (event.target === cy) {
+      destroySideTab();
+  }
+});
+
+cy.on('click', 'node', function(event) {
+  // Empêche l'exécution de maFonction si un node est cliqué
+  event.stopPropagation();
+});
+
+
